fee219c159e52c6875989bebf957420f143c1674
kelby-amerson <kelby.amerson@yahoo.com>
2024-11-10 21:54:13 -0500

    updating ssr to false and not rendering on server

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,4 +1,5 @@
-/** @type {import('./$types').PageServerLoad} */
+export const ssr = false;
+/** @type {import('./$types').PageLoad} */
 export async function load ({ fetch }){
     /**
 	 * @param {number} max
